007d8b75fb0eddbc713ef3b28e3c0876dab2d1dd
Tony Russo <ynotrusso@gmail.com>
2022-09-30 16:39:36 +0700

    basic twitter request via axios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+	// Twitter
+
+	let searchResults = await axios.get('https://api.twitter.com/2/tweets/search/recent?query=war')
+
+	console.log(searchResults)
+
+	// Watson
+
+	// Render
+  res.render('index', { title: 'Express', searchResults });
 });
 
 module.exports = router;
