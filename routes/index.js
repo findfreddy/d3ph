aac8d8c4601bf1b767a65a75cef5c6890e6bc3cd
findfreddy <63041123+findfreddy@users.noreply.github.com>
2022-09-30 20:13:06 +0700

    1st safety commit
    FUCK CUNT FUCK

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const axios = require('axios')
+const axios = require("axios");
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-	// Twitter
+router.get("/", async function(req, res, next) {
+  // Twitter
 
-	let searchResults = await axios.get('https://api.twitter.com/2/tweets/search/recent?query=war')
+  // let searchResults = await axios.get(
+  //   "https://api.twitter.com/2/tweets/search/recent?query=war' headers: {'Authorization': Bearer AAAAAAAAAAAAAAAAAAAAAOwshgEAAAAAHriLJShnkBSUOnAlW1BbbRH5DOA%3DvWhH5ttyxXl7g33krHz6m6IPjeL57HLh8SQ76QBPR8yvin2SeP});
 
-	console.log(searchResults)
+  // console.log(searchResults);
 
-	// Watson
+  // Watson
 
-	// Render
-  res.render('index', { title: 'Express', searchResults });
+  // Render
+  res.render("index", { title: "Express", searchResults });
 });
 
 module.exports = router;
